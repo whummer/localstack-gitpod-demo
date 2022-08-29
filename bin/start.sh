d80f348eb9f9f0911ee5d8fc5cfa64a37c055dc7
Waldemar Hummer <waldemar.hummer@gmail.com>
2022-08-29 10:44:59 +0200

    update demo app config

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # enable browser access from demo app to local APIs
-export EXTRA_CORS_ALLOWED_ORIGINS=http://localhost:3000
+export EXTRA_CORS_ALLOWED_ORIGINS='*'
+export DISABLE_CORS_CHECKS=1
+export DISABLE_CUSTOM_CORS_APIGATEWAY=1
 
 # start LocalStack container in the background
 DEBUG=1 localstack start -d
